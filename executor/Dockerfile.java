c7cb4d4c4342f3e9d3106588055e31f17d987c11
Isha Kumar <70069912+Isha-pixel@users.noreply.github.com>
2025-08-20 03:06:23 +0530

    Update Dockerfile.java

--- executor/Dockerfile.java
+++ executor/Dockerfile.java
@@ -1,14 +1,3 @@
-# FROM eclipse-temurin:17-jdk-jammy
-
-# RUN useradd -m coder
-# WORKDIR /app
-# USER coder
-
-# CMD ["bash","-lc","echo java ready"]
-
-
-
-
 FROM eclipse-temurin:17-jdk-jammy
 RUN useradd -m coder
 WORKDIR /app
