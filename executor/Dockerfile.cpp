d9dae194104eb99ddc90d25a32d441a367d5cff0
Isha Kumar <70069912+Isha-pixel@users.noreply.github.com>
2025-08-20 03:05:58 +0530

    Update Dockerfile.cpp

--- executor/Dockerfile.cpp
+++ executor/Dockerfile.cpp
@@ -1,15 +1,3 @@
-# FROM gcc:13
-
-# # create unprivileged user
-# RUN useradd -m coder
-# WORKDIR /app
-# USER coder
-
-# # default (we pass the command from the host)
-# CMD ["bash","-lc","echo cpp ready"]
-
-
-
 FROM gcc:13
 RUN useradd -m coder
 WORKDIR /app
